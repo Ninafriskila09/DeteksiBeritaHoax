0aa6676c22192b53d400b072fed8f417e1ea4ea0
nina <162159487+Ninafriskila09@users.noreply.github.com>
2024-08-07 01:05:50 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,43 +7,46 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import classification_report, confusion_matrix
 from scipy.sparse import csr_matrix
 from wordcloud import WordCloud
-import joblib
 
-# Nama file dataset dan model
-DATASET_FILE = 'dataset_clean.xlsx'
-MODEL_FILE = 'naive_bayes_model.joblib'
-VECTORIZER_FILE = 'tfidf_vectorizer.joblib'
-CHI2_FEATURES_FILE = 'chi2_features.joblib'
+# Fungsi untuk membaca data dan melakukan preprocessing
 
-# Fungsi untuk membaca data
+
 def load_data(file_path):
-    return pd.read_excel(file_path)
+    data = pd.read_excel(file_path)
+    return data
 
 # Fungsi untuk melakukan pemrosesan data
-def preprocess_data(data, vectorizer, chi2_features):
+
+
+def preprocess_data(data):
     X_raw = data["clean_text"]
     y_raw = data["Label"]
-
     X_train, X_test, y_train, y_test = train_test_split(
-        X_raw, y_raw, test_size=0.2, random_state=42
-    )
+        X_raw, y_raw, test_size=0.2, random_state=42)
 
+    vectorizer = TfidfVectorizer(ngram_range=(1, 2))
+    vectorizer.fit(X_train)
     X_train_TFIDF = vectorizer.transform(X_train)
     X_test_TFIDF = vectorizer.transform(X_test)
 
-    X_train_kbest = chi2_features.transform(X_train_TFIDF)
-    X_test_kbest = chi2_features.transform(X_test_TFIDF)
+    chi2_features = SelectKBest(chi2, k=500)
+    X_kbest_features = chi2_features.fit_transform(X_train_TFIDF, y_train)
 
-    return X_train_kbest, y_train, X_test_kbest, y_test
+    return X_kbest_features, y_train, X_test_TFIDF, y_test, vectorizer, chi2_features
 
 # Fungsi untuk melatih model
+
+
 def train_model(X_train, y_train):
     NB = GaussianNB()
+    # Mengonversi matriks sparse menjadi matriks padat
     X_train_dense = csr_matrix.toarray(X_train)
     NB.fit(X_train_dense, y_train)
     return NB
 
 # Fungsi untuk menampilkan hasil evaluasi
+
+
 def display_evaluation(y_test, y_pred):
     st.write("Classification Report:")
     st.text(classification_report(y_test, y_pred))
@@ -55,54 +58,60 @@ def display_evaluation(y_test, y_pred):
     st.write("Confusion Matrix:")
     st.write(df_cm)
 
+
 def main():
     st.title("Aplikasi Klasifikasi Sentimen")
 
-    # Load dataset
-    data = load_data(DATASET_FILE)
+    # Upload file dataset
+    st.write("Upload file dataset:")
+    uploaded_file = st.file_uploader(
+        "Pilih file CSV atau Excel", type=['csv', 'xlsx'])
 
-    # Load model and preprocessing tools
-    model = joblib.load(MODEL_FILE)
-    vectorizer = joblib.load(VECTORIZER_FILE)
-    chi2_features = joblib.load(CHI2_FEATURES_FILE)
+    if uploaded_file is not None:
+        data = load_data(uploaded_file)
+        X_train, y_train, X_test, y_test, vectorizer, chi2_features = preprocess_data(
+            data)
+        model = train_model(X_train, y_train)
 
-    # Preprocessing data
-    X_train, y_train, X_test, y_test = preprocess_data(data, vectorizer, chi2_features)
+        # Evaluasi model
+        X_test_chi2 = chi2_features.transform(X_test)
+        # Konversi matriks sparse menjadi matriks padat
+        X_test_chi2_dense = csr_matrix.toarray(X_test_chi2)
+        y_pred = model.predict(X_test_chi2_dense)
+        display_evaluation(y_test, y_pred)
 
-    # Evaluasi model
-    X_test_dense = csr_matrix.toarray(X_test)
-    y_pred = model.predict(X_test_dense)
-    display_evaluation(y_test, y_pred)
+        # Tampilkan Word Cloud
+        st.write("Word Cloud untuk Semua Data:")
+        all_text = ' '.join(data['clean_text'])
+        wordcloud_all = WordCloud(
+            width=800, height=400, background_color='white').generate(all_text)
+        st.image(wordcloud_all.to_array(), use_column_width=True)
 
-    # Tampilkan Word Cloud
-    st.write("Word Cloud untuk Semua Data:")
-    all_text = ' '.join(data['clean_text'])
-    wordcloud_all = WordCloud(width=800, height=400, background_color='white').generate(all_text)
-    st.image(wordcloud_all.to_array(), use_column_width=True)
+        st.write("Word Cloud untuk Fakta:")
+        fakta = data[data['Label'] == 1]
+        all_text_fakta = ' '.join(fakta['clean_text'])
+        wordcloud_fakta = WordCloud(
+            width=800, height=400, background_color='white').generate(all_text_fakta)
+        st.image(wordcloud_fakta.to_array(), use_column_width=True)
 
-    st.write("Word Cloud untuk Fakta:")
-    fakta = data[data['Label'] == 1]
-    all_text_fakta = ' '.join(fakta['clean_text'])
-    wordcloud_fakta = WordCloud(width=800, height=400, background_color='white').generate(all_text_fakta)
-    st.image(wordcloud_fakta.to_array(), use_column_width=True)
+        st.write("Word Cloud untuk Hoax:")
+        hoax = data[data['Label'] == 0]
+        all_text_hoax = ' '.join(hoax['clean_text'])
+        wordcloud_hoax = WordCloud(
+            width=800, height=400, background_color='white').generate(all_text_hoax)
+        st.image(wordcloud_hoax.to_array(), use_column_width=True)
 
-    st.write("Word Cloud untuk Hoax:")
-    hoax = data[data['Label'] == 0]
-    all_text_hoax = ' '.join(hoax['clean_text'])
-    wordcloud_hoax = WordCloud(width=800, height=400, background_color='white').generate(all_text_hoax)
-    st.image(wordcloud_hoax.to_array(), use_column_width=True)
+        # Input teks untuk diprediksi
+        st.write("Masukkan teks untuk diprediksi:")
+        input_text = st.text_input("Teks", "")
+        if input_text:
+            input_text_tfidf = vectorizer.transform([input_text])
+            input_text_chi2 = chi2_features.transform(input_text_tfidf)
+            input_text_chi2_dense = csr_matrix.toarray(input_text_chi2)
+            prediction = model.predict(input_text_chi2_dense)
+            sentiment = "Fakta" if prediction[0] == 0 else "Hoax"
+            st.write("Hasil prediksi:", sentiment)
 
-    # Input teks untuk diprediksi
-    st.write("Masukkan teks untuk diprediksi:")
-    input_text = st.text_input("Teks", "")
-    if input_text:
-        input_text_tfidf = vectorizer.transform([input_text])
-        input_text_chi2 = chi2_features.transform(input_text_tfidf)
-        input_text_chi2_dense = csr_matrix.toarray(input_text_chi2)
-        prediction = model.predict(input_text_chi2_dense)
-        sentiment = "Fakta" if prediction[0] == 1 else "Hoax"
-        st.write("Hasil prediksi:", sentiment)
 
 if __name__ == '__main__':
     main()
-
