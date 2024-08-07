58415bb64d64056b9261e842b518d72ede5341bd
nina <162159487+Ninafriskila09@users.noreply.github.com>
2024-08-07 19:42:21 +0700

    Update app.py

--- app.py
+++ app.py
@@ -61,44 +61,6 @@ def main():
     st.title("Aplikasi Klasifikasi Sentimen")
     st.write("def")
 
-# Evaluasi model
-        X_test_chi2 = chi2_features.transform(X_test)
-        # Konversi matriks sparse menjadi matriks padat
-        X_test_chi2_dense = csr_matrix.toarray(X_test_chi2)
-        y_pred = model.predict(X_test_chi2_dense)
-        display_evaluation(y_test, y_pred)
-
-        # Tampilkan Word Cloud
-        st.write("Word Cloud untuk Semua Data:")
-        all_text = ' '.join(data['clean_text'])
-        wordcloud_all = WordCloud(
-            width=800, height=400, background_color='white').generate(all_text)
-        st.image(wordcloud_all.to_array(), use_column_width=True)
-
-        st.write("Word Cloud untuk Fakta:")
-        fakta = data[data['Label'] == 1]
-        all_text_fakta = ' '.join(fakta['clean_text'])
-        wordcloud_fakta = WordCloud(
-            width=800, height=400, background_color='white').generate(all_text_fakta)
-        st.image(wordcloud_fakta.to_array(), use_column_width=True)
-
-        st.write("Word Cloud untuk Hoax:")
-        hoax = data[data['Label'] == 0]
-        all_text_hoax = ' '.join(hoax['clean_text'])
-        wordcloud_hoax = WordCloud(
-            width=800, height=400, background_color='white').generate(all_text_hoax)
-        st.image(wordcloud_hoax.to_array(), use_column_width=True)
-
-        # Input teks untuk diprediksi
-        st.write("Masukkan teks untuk diprediksi:")
-        input_text = st.text_input("Teks", "")
-        if input_text:
-            input_text_tfidf = vectorizer.transform([input_text])
-            input_text_chi2 = chi2_features.transform(input_text_tfidf)
-            input_text_chi2_dense = csr_matrix.toarray(input_text_chi2)
-            prediction = model.predict(input_text_chi2_dense)
-            sentiment = "Fakta" if prediction[0] == 0 else "Hoax"
-            st.write("Hasil prediksi:", sentiment)
 
 
 if __name__ == '__main__':
