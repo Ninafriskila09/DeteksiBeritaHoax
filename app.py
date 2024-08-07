e58d45ba51fb6071b1bb504242d1a234adbd70ad
nina <162159487+Ninafriskila09@users.noreply.github.com>
2024-08-07 18:36:39 +0700

    Update app.py

--- app.py
+++ app.py
@@ -8,14 +8,7 @@ with open ('hoax_pickle', 'rb') as file:
 # Input teks untuk diprediksi
 st.write("Masukkan teks untuk diprediksi:")
 input_text = st.text_input("Teks", "")
-        if input_text:
-            input_text_tfidf = vectorizer.transform([input_text])
-            input_text_chi2 = chi2_features.transform(input_text_tfidf)
-            input_text_chi2_dense = csr_matrix.toarray(input_text_chi2)
-            prediction = model.predict(input_text_chi2_dense)
-            sentiment = "Fakta" if prediction[0] == 0 else "Hoax"
-            st.write("Hasil prediksi:", sentiment)
-    
+       
 
         
 
