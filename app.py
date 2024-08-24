a9fe49887d3d969008ab38149a0db5f922dbbcf3
nina <162159487+Ninafriskila09@users.noreply.github.com>
2024-08-24 16:08:51 +0700

    Update app.py

--- app.py
+++ app.py
@@ -33,14 +33,10 @@ def train_model(X_train, y_train):
     return NB
 
 def display_evaluation(y_test, y_pred):
-    # Menyiapkan confusion matrix
-    columns = sorted(y_test.unique())
     confm = confusion_matrix(y_test, y_pred, labels=columns)
     df_cm = pd.DataFrame(confm, index=columns, columns=columns)
     
 def display_evaluation(y_test, y_pred):
-    columns = sorted(y_test.unique())
-    confm = confusion_matrix(y_test, y_pred, labels=columns)
     df_cm = pd.DataFrame(confm, index=columns, columns=columns)
     st.write("Classification Report:")
     st.text(classification_report(y_test, y_pred))
