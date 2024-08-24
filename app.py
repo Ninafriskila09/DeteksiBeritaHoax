3e8e1403055a2212e250db04affb65d8387e7d1f
nina <162159487+Ninafriskila09@users.noreply.github.com>
2024-08-24 21:37:10 +0700

    Update app.py

--- app.py
+++ app.py
@@ -54,11 +54,15 @@ def display_wordclouds(data):
     wordcloud_all = WordCloud(width=800, height=400, background_color='white').generate(all_text)
     st.image(wordcloud_all.to_array(), use_column_width=True)
 
+    st.markdown("<br>", unsafe_allow_html=True)
+
     st.write("**Word Cloud untuk Fakta:**")
     fakta = data[data['Label'] == 1]
     all_text_fakta = ' '.join(fakta['clean_text'])
     wordcloud_fakta = WordCloud(width=800, height=400, background_color='white').generate(all_text_fakta)
     st.image(wordcloud_fakta.to_array(), use_column_width=True)
+
+    st.markdown("<br>", unsafe_allow_html=True)
 
     st.write("**Word Cloud untuk Hoax:**")
     hoax = data[data['Label'] == 0]
