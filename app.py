2f64493981481ef8058b32997347c2471bccdb96
nina <162159487+Ninafriskila09@users.noreply.github.com>
2024-08-25 00:05:18 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import joblib
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.naive_bayes import MultinomialNB
+from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import classification_report, confusion_matrix
 from wordcloud import WordCloud
 from scipy.sparse import csr_matrix
@@ -27,19 +27,25 @@ def preprocess_data(data):
     return X_TFIDF, y_raw, vectorizer
 
 def train_model(X_train, y_train):
-    NB = MultinomialNB()
-    NB.fit(X_train, y_train)
+    NB = GaussianNB()
+    X_train_dense = csr_matrix.toarray(X_train)
+    NB.fit(X_train_dense, y_train)
     return NB
 
 def display_evaluation(y_test, y_pred):
-    st.write("**Classification Report:**")
+    confm = confusion_matrix(y_test, y_pred, labels=columns)
+    df_cm = pd.DataFrame(confm, index=columns, columns=columns)
+    
+def display_evaluation(y_test, y_pred):
+    df_cm = pd.DataFrame(confm, index=columns, columns=columns)
+    st.write("Classification Report:")
     st.text(classification_report(y_test, y_pred))
 
     columns = sorted(y_test.unique())
     confm = confusion_matrix(y_test, y_pred, labels=columns)
     df_cm = pd.DataFrame(confm, index=columns, columns=columns)
 
-    st.write("**Confusion Matrix:**")
+    st.write("Confusion Matrix:")
     st.write(df_cm)
 
 def display_wordclouds(data):
@@ -96,34 +102,27 @@ def main():
 
             # Transformasi teks dengan vectorizer yang digunakan untuk melatih model
             input_text_tfidf = vectorizer.transform([input_text])
+            input_text_dense = csr_matrix.toarray(input_text_tfidf)
 
-            # Prediksi dan probabilitas menggunakan model yang telah dilatih
-            prediction_probabilities = model.predict_proba(input_text_tfidf)
-            prediction = model.predict(input_text_tfidf)
-
-            # Mendapatkan probabilitas untuk setiap kelas
-            probability_fakta = prediction_probabilities[0][1]  # Probabilitas untuk kelas 1
-            probability_hoax = prediction_probabilities[0][0]   # Probabilitas untuk kelas 0
-
-            sentiment = "Fakta" if prediction[0] == 1 else "Hoax"
+            # Prediksi menggunakan model yang telah dimuat
+            prediction = model.predict(input_text_dense)
+            sentiment = "Fakta" if prediction[0] == 0 else "Hoax"
 
             # Menampilkan hasil
             color = "green" if sentiment == "Fakta" else "red"
             st.markdown(f"""
-            <div style="text-align: center; background-color: {color}; color: white; padding: 10px;">
-                <strong>{sentiment}</strong><br>
-                <span>Fakta: {probability_fakta * 100:.2f}%</span><br>
-                <span>Hoax: {probability_hoax * 100:.2f}%</span>
-            </div>
-            """, unsafe_allow_html=True)
-
+    <div style="text-align: center; background-color: {color}; color: white; padding: 10px;">
+        <strong>{sentiment}</strong>
+    </div>
+    """, unsafe_allow_html=True)
+            
     elif menu == "Evaluasi Model":
         # Memisahkan data untuk pelatihan dan pengujian
         X_train, X_test, y_train, y_test = train_test_split(X_features, y_labels, test_size=0.2, random_state=42)
         model = train_model(X_train, y_train)
 
         # Evaluasi model
-        y_pred = model.predict(X_test)
+        y_pred = model.predict(csr_matrix.toarray(X_test))
         display_evaluation(y_test, y_pred)
 
     elif menu == "Visualisasi Word Cloud":
