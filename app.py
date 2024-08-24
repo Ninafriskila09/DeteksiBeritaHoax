011cd08a45620c7742ff8e8e43f2e0ac284ae869
nina <162159487+Ninafriskila09@users.noreply.github.com>
2024-08-24 11:20:48 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import joblib
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.naive_bayes import GaussianNB
+from sklearn.naive_bayes import MultinomialNB  # Ubah ke MultinomialNB
 from sklearn.metrics import classification_report, confusion_matrix
 from wordcloud import WordCloud
 from scipy.sparse import csr_matrix
@@ -27,9 +27,8 @@ def preprocess_data(data):
     return X_TFIDF, y_raw, vectorizer
 
 def train_model(X_train, y_train):
-    NB = GaussianNB()
-    X_train_dense = csr_matrix.toarray(X_train)
-    NB.fit(X_train_dense, y_train)
+    NB = MultinomialNB()  # Ubah ke MultinomialNB
+    NB.fit(X_train, y_train)
     return NB
 
 def display_evaluation(y_test, y_pred):
@@ -97,11 +96,17 @@ def main():
 
             # Transformasi teks dengan vectorizer yang digunakan untuk melatih model
             input_text_tfidf = vectorizer.transform([input_text])
-            input_text_dense = csr_matrix.toarray(input_text_tfidf)
-
-            # Prediksi menggunakan model yang telah dimuat
-            prediction = model.predict(input_text_dense)
-            sentiment = "Fakta" if prediction[0] == 0 else "Hoax"
+            
+            # Prediksi probabilitas
+            probas = model.predict_proba(input_text_tfidf)
+            probas_df = pd.DataFrame(probas, columns=model.classes_)
+            probas_df = probas_df.T.reset_index()
+            probas_df.columns = ['Label', 'Probability']
+            probas_df = probas_df.sort_values(by='Probability', ascending=False)
+            
+            # Prediksi kelas
+            prediction = model.predict(input_text_tfidf)
+            sentiment = "Fakta" if prediction[0] == 1 else "Hoax"
 
             # Menampilkan hasil
             color = "green" if sentiment == "Fakta" else "red"
@@ -110,14 +115,18 @@ def main():
         <strong>{sentiment}</strong>
     </div>
     """, unsafe_allow_html=True)
-            
+
+            # Menampilkan presentasi probabilitas
+            st.write("**Probabilitas Kelas:**")
+            st.table(probas_df)
+
     elif menu == "Evaluasi Model":
         # Memisahkan data untuk pelatihan dan pengujian
         X_train, X_test, y_train, y_test = train_test_split(X_features, y_labels, test_size=0.2, random_state=42)
         model = train_model(X_train, y_train)
 
         # Evaluasi model
-        y_pred = model.predict(csr_matrix.toarray(X_test))
+        y_pred = model.predict(X_test)
         display_evaluation(y_test, y_pred)
 
     elif menu == "Visualisasi Word Cloud":
