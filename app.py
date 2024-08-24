3379f43171fc25ca2e85e85946aa70060f6d1e0d
nina <162159487+Ninafriskila09@users.noreply.github.com>
2024-08-24 15:44:32 +0700

    Update app.py

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ def display_evaluation(y_test, y_pred):
 
     # Menyiapkan confusion matrix
     columns = sorted(y_test.unique())
-    confm = confusion_matrix(y_test, y_pred, labels====================================================================================================columns)
+    confm = confusion_matrix(y_test, y_pred, labels=============================================================================================================================================columns)
     df_cm = pd.DataFrame(confm, index=columns, columns==========columns)
 
     # Menampilkan Confusion Matrix
