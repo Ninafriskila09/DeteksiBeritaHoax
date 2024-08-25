c0d1d42c7d23cbb77bc8ec0b9a859f60398112d9
nina <162159487+Ninafriskila09@users.noreply.github.com>
2024-08-25 21:20:20 +0700

    Update app.py

--- app.py
+++ app.py
@@ -33,6 +33,16 @@ def train_model(X_train, y_train):
     NB.fit(X_train_dense, y_train)
     return NB
 
+def display_evaluation(y_test, y_pred):
+    st.write("Classification Report:")
+    st.text(classification_report(y_test, y_pred))
+
+    columns = sorted(y_test.unique())
+    confm = confusion_matrix(y_test, y_pred, labels=columns)
+    df_cm = pd.DataFrame(confm, index=columns, columns=columns)
+
+    st.write("Confusion Matrix:")
+    st.write(df_cm)
 
 
 
