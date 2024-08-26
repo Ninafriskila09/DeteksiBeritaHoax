8ba8fbeaa28ef27415e1e31e5989021fc539ce6d
nina <162159487+Ninafriskila09@users.noreply.github.com>
2024-08-26 15:02:00 +0700

    Update app.py

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ def display_wordclouds(data):
     st.markdown("<br>", unsafe_allow_html=True)
 
     st.write("**Word Cloud untuk Fakta:**")
-    fakta = data[data['Label'] == 0]
+    fakta = data[data['Label'] == 1]
     all_text_fakta = ' '.join(fakta['clean_text'])
     wordcloud_fakta = WordCloud(width=800, height=400, background_color='white').generate(all_text_fakta)
     st.image(wordcloud_fakta.to_array(), use_column_width=True)
@@ -61,7 +61,7 @@ def display_wordclouds(data):
     st.markdown("<br>", unsafe_allow_html=True)
 
     st.write("**Word Cloud untuk Hoax:**")
-    hoax = data[data['Label'] == 1]
+    hoax = data[data['Label'] == 0]
     all_text_hoax = ' '.join(hoax['clean_text'])
     wordcloud_hoax = WordCloud(width=800, height=400, background_color='white').generate(all_text_hoax)
     st.image(wordcloud_hoax.to_array(), use_column_width=True)
