95c81f41282b8f41979242886e5fb0337b844506
nina <162159487+Ninafriskila09@users.noreply.github.com>
2024-08-26 22:15:22 +0700

    Update app.py

--- app.py
+++ app.py
@@ -34,18 +34,18 @@ def train_model(X_train, y_train):
     return NB
 
 def display_evaluation(y_test, y_pred):
-    st.write("**Classification Report:**")
+    st.write("### Classification Report")
     st.text(classification_report(y_test, y_pred))
 
     columns = sorted(y_test.unique())
     confm = confusion_matrix(y_test, y_pred, labels=columns)
     df_cm = pd.DataFrame(confm, index=columns, columns=columns)
 
-    st.write("**Confusion Matrix:**")
+    st.write("### Confusion Matrix")
     st.write(df_cm)
 
 def display_wordclouds(data):
-    st.write("**Word Cloud untuk Semua Data:**")
+    st.write("### Word Cloud untuk Semua Data")
     all_text = ' '.join(data['clean_text'])
     wordcloud_all = WordCloud(width=800, height=400, background_color='white').generate(all_text)
     plt.figure(figsize=(10, 5))
@@ -53,10 +53,8 @@ def display_wordclouds(data):
     plt.axis('off')
     st.pyplot()
 
-    st.markdown("<br>", unsafe_allow_html=True)
-
-    st.write("**Word Cloud untuk Fakta:**")
-    fakta = data[data['Label'] == 0]  # Fakta adalah label 0
+    st.write("### Word Cloud untuk Fakta")
+    fakta = data[data['Label'] == 0]
     all_text_fakta = ' '.join(fakta['clean_text'])
     wordcloud_fakta = WordCloud(width=800, height=400, background_color='white').generate(all_text_fakta)
     plt.figure(figsize=(10, 5))
@@ -64,10 +62,8 @@ def display_wordclouds(data):
     plt.axis('off')
     st.pyplot()
 
-    st.markdown("<br>", unsafe_allow_html=True)
-
-    st.write("**Word Cloud untuk Hoax:**")
-    hoax = data[data['Label'] == 1]  # Hoax adalah label 1
+    st.write("### Word Cloud untuk Hoax")
+    hoax = data[data['Label'] == 1]
     all_text_hoax = ' '.join(hoax['clean_text'])
     wordcloud_hoax = WordCloud(width=800, height=400, background_color='white').generate(all_text_hoax)
     plt.figure(figsize=(10, 5))
@@ -76,7 +72,6 @@ def display_wordclouds(data):
     st.pyplot()
 
 def main():
-    # Mengubah background menjadi putih dengan CSS
     st.markdown(
         """
         <style>
@@ -100,13 +95,11 @@ def main():
         unsafe_allow_html=True
     )
 
-    st.markdown("<h2 style='text-align: center; color: #007bff;'>Sistem Deteksi Berita Hoax Naive Bayes</h2>",
+    st.markdown("<h1 style='text-align: center; color: #007bff;'>Sistem Deteksi Berita Hoax Naive Bayes</h1>",
                 unsafe_allow_html=True)
 
-    # Sidebar menu
     menu = st.sidebar.radio("Pilih Menu", ["Deteksi Berita", "Evaluasi Model", "Visualisasi Word Cloud"])
 
-    # Load data dan preprocess
     data = load_data()
     X_features, y_labels, vectorizer = preprocess_data(data)
 
@@ -121,23 +114,18 @@ def main():
             detect_button = st.button("Deteksi")
 
         if detect_button and input_text:
-            # Memisahkan data untuk pelatihan dan pengujian
             X_train, X_test, y_train, y_test = train_test_split(X_features, y_labels, test_size=0.2, random_state=42)
             model = train_model(X_train, y_train)
 
-            # Transformasi teks dengan vectorizer yang digunakan untuk melatih model
             input_text_tfidf = vectorizer.transform([input_text])
             input_text_dense = csr_matrix.toarray(input_text_tfidf)
 
-            # Prediksi menggunakan model yang telah dimuat
             prediction = model.predict(input_text_dense)
 
-            # Menghitung probabilitas untuk setiap kelas
             probabilities = model.predict_proba(input_text_dense)
-            prob_fakta = probabilities[0][0] * 100  # Probabilitas "Fakta"
-            prob_hoax = probabilities[0][1] * 100   # Probabilitas "Hoax"
+            prob_fakta = probabilities[0][0] * 100
+            prob_hoax = probabilities[0][1] * 100
 
-            # Menampilkan hasil
             sentiment = "Fakta" if prediction[0] == 0 else "Hoax"
             color = "green" if sentiment == "Fakta" else "red"
 
@@ -147,22 +135,10 @@ def main():
     </div>
     """, unsafe_allow_html=True)
 
-            # Display probabilities
             st.write(f"**Probabilitas Fakta:** {prob_fakta:.2f}%")
             st.write(f"**Probabilitas Hoax:** {prob_hoax:.2f}%")
 
     elif menu == "Evaluasi Model":
-        # Memisahkan data untuk pelatihan dan pengujian
         X_train, X_test, y_train, y_test = train_test_split(X_features, y_labels, test_size=0.2, random_state=42)
         model = train_model(X_train, y_train)
-
-        # Evaluasi model
-        y_pred = model.predict(csr_matrix.toarray(X_test))
-        display_evaluation(y_test, y_pred)
-
-    elif menu == "Visualisasi Word Cloud":
-        # Tampilkan Word Cloud di bawah hasil
-        display_wordclouds(data)
-
-if __name__ == '__main__':
-    main()
+        y_pred
