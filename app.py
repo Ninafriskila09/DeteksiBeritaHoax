deb49ac1c0b38069d3fc3f0cb8ccc936132b8add
nina <162159487+Ninafriskila09@users.noreply.github.com>
2024-08-28 16:32:48 +0700

    Update app.py

--- app.py
+++ app.py
@@ -66,7 +66,7 @@ def load_html():
         return file.read()
 
 def load_css():
-    css_file_path = "path/to/your/styles.css"  # Ganti dengan path yang benar
+    css_file_path = "path/to/your/style.css"  # Ganti dengan path yang benar
     try:
         with open(css_file_path, "r") as file:
             return file.read()
